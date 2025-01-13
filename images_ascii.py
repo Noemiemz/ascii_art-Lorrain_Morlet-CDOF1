35c167eb663f0af16d70f896189dd4c523faacd7
Noémie Mazepa <noemie.mazepa@edu.devinci.fr>
2025-01-13 16:29:02 +0100

    comments on main code
    For a clearer understanding of the code.

--- images_ascii.py
+++ images_ascii.py
@@ -1,25 +1,34 @@
 import cv2
 import numpy as np
 
+# Path to the input image
 image_path = 'images/monalisa.jpg'
 image = cv2.imread(image_path)
 
+# Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# Convert the grayscale image to a NumPy array
 intensity_matrix = np.array(gray_image)
 
+# Define the ASCII characters to represent different intensity levels
 densities = " ░▒▓█"
 
+# Normalize the intensity matrix to the range of the densities length
 intensity_matrix = intensity_matrix / (intensity_matrix.max() / (len(densities) -1))
+# Convert the normalized intensity values to integers
 intensity_matrix = intensity_matrix.astype(int)
 
 
-
+# Open a file to write the ASCII art
 with open('images/monalisa.txt', 'w', encoding='utf-8') as f:
+    # Iterate over each intensity value in each row
     for row in intensity_matrix:
         for intensity in row:
+            # Print the corresponding ASCII character to the console
             print(densities[intensity], end='')
+            # Write the corresponding ASCII character to the file
             f.write(densities[intensity])
+        
         f.write('\n')
         print()
-
